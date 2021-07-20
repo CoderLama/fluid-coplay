8278de7c896b37a283efab3f280fbe19eeff0359
CoderLama <coderlamagithub@gmail.com>
2021-07-20 15:36:31 +0530

    Refactored

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,3 @@
-/*!
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
- */
 
 import { getTinyliciousContainer } from "@fluidframework/get-tinylicious-container";
 import { getDefaultObjectFromContainer } from "@fluidframework/aqueduct";
